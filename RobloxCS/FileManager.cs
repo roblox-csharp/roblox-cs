0c7b1b9439c4b9b212c2dabbfd4150134b3cf051
R-unic <roleyspersonal@gmail.com>
2024-07-07 19:15:41 -0400

    feat: set up file reading

---
+++ RobloxCS/FileManager.cs
@@ -0,0 +1,24 @@
+﻿namespace RobloxCS
+{
+    internal static class FileManager
+    {
+        public static string[] GetSourceFiles(string sourceDirectory)
+        {
+            try
+            {
+                return Directory.GetFiles(sourceDirectory, "*.cs", SearchOption.AllDirectories);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to read source files:");
+                Console.WriteLine(e.Message);
+                return [];
+            }
+        }
+
+        public static bool DirectoryExists(string directory)
+        {
+            return 
+        }
+    }
+}
---
+++ RobloxCS/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RobloxCS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string? inputtedDirectory = args.FirstOrDefault();
+            if (inputtedDirectory != null && !Directory.Exists(inputtedDirectory))
+            {
+                throw new Exception($"Directory \"{inputtedDirectory}\" does not exist.");
+            }
+
+            bool hasSrcDirectory = Directory.Exists("src");
+            string sourceDirectory = inputtedDirectory == null ? (hasSrcDirectory ? "src" : ".") : inputtedDirectory;
+            string[] sourceFiles = FileManager.GetSourceFiles(sourceDirectory);
+            foreach (var sourceFile in sourceFiles)
+            {
+                var fileContents = File.ReadAllText(sourceFile);
+            }
+        }
+    }
+}
