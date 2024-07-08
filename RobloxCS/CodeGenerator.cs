929a16eb75f3f56ae477ddae867383e3f06072d6
R-unic <roleyspersonal@gmail.com>
2024-07-07 21:47:35 -0400

    feat: compile class declarations

--- RobloxCS/CodeGenerator.cs
+++ RobloxCS/CodeGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 
@@ -55,15 +56,50 @@ namespace RobloxCS
             WriteLine("end");
         }
 
-        private void WriteLine(string text)
+        public override void VisitClassDeclaration(ClassDeclarationSyntax node)
         {
-            _output.AppendLine(GetTabString() + text);
+            var parentNamespace = FindFirstAncestor<NamespaceDeclarationSyntax>(node);
+            string namespaceName = string.Join(".", GetNames(parentNamespace));
+            var className = GetNames(node).First();
+            var luaClassName = namespaceName + (namespaceName == "" ? "" : ".") + className;
+            Write(luaClassName);
+            WriteLine(" = {} do");
+            _indent++;
 
+            WriteLine($"local {className} = {luaClassName}");
+            if (!HasSyntax(node.Modifiers, SyntaxKind.StaticKeyword))
+            {
+                WriteLine($"{className}.__index = {className}");
+            }
+
+            PrintChildNodes(node);
+            base.VisitClassDeclaration(node);
+
+            _indent--;
+            WriteLine("end");
+        }
+
+        private void WriteLine(string? text = null)
+        {
+            if (text == null)
+            {
+                _output.AppendLine();
+                return;
+            }
+
+            WriteTab();
+            _output.AppendLine(text);
         }
 
         private void Write(string text)
         {
-            _output.Append(GetTabString() + text);
+            WriteTab();
+            _output.Append(text);
+        }
+
+        private void WriteTab()
+        {
+            _output.Append(GetLastCharacter() == '\n' ? GetTabString() : "");
         }
 
         private string GetTabString()
@@ -71,9 +107,29 @@ namespace RobloxCS
             return string.Concat(Enumerable.Repeat(" ", _indentSize * _indent));
         }
 
-        private List<string> GetNames(SyntaxNode node)
+        private bool HasSyntax(SyntaxTokenList tokens, SyntaxKind syntax)
+        {
+            return tokens.Any(token => token.IsKind(syntax));
+        }
+
+        private T? FindFirstAncestor<T>(SyntaxNode node) where T : SyntaxNode
+        {
+            return node.Ancestors().OfType<T>().FirstOrDefault();
+        }
+
+        private List<string> GetNames(SyntaxNode? node)
         {
             var names = new List<string>();
+            if (node == null) return names;
+
+            var identifierProperty = node.GetType().GetProperty("Identifier");
+            var identifierValue = identifierProperty?.GetValue(node);
+            if (identifierProperty != null && identifierValue != null && identifierValue is SyntaxToken)
+            {
+                names.Add(((SyntaxToken)identifierValue).Text.Trim());
+                return names;
+            }
+
             var childNodes = node.ChildNodes();
             var qualifiedNameNodes = node.IsKind(SyntaxKind.QualifiedName) ? [(QualifiedNameSyntax)node] : childNodes.OfType<QualifiedNameSyntax>();
             var identifierNameNodes = node.IsKind(SyntaxKind.IdentifierName) ? [(IdentifierNameSyntax)node] : childNodes.OfType<IdentifierNameSyntax>();
@@ -82,20 +138,44 @@ namespace RobloxCS
             {
                 foreach (var name in GetNames(qualifiedNameNode.Left))
                 {
-                    names.Add(name);
+                    names.Add(name.Trim());
                 }
                 foreach (var name in GetNames(qualifiedNameNode.Right))
                 {
-                    names.Add(name);
+                    names.Add(name.Trim());
                 }
             }
 
             foreach (var identifierNameNode in identifierNameNodes)
             {
-                names.Add(identifierNameNode.Identifier.Text);
+                names.Add(identifierNameNode.Identifier.Text.Trim());
             }
 
             return names;
+        }
+
+        private char? GetLastCharacter()
+        {
+            if (_output.Length == 0) return null;
+            return _output[_output.Length - 1];
+        }
+
+        private void PrintChildNodes(SyntaxNode node)
+        {
+            Console.WriteLine($"{node.Kind()} node children: {node.ChildNodes().Count()}");
+            foreach (var child in node.ChildNodes())
+            {
+                Console.WriteLine(child.Kind().ToString() + ": " + child.GetText());
+            }
+        }
+
+        private void PrintChildTokens(SyntaxNode node)
+        {
+            Console.WriteLine($"{node.Kind()} token children: {node.ChildTokens().Count()}");
+            foreach (var child in node.ChildTokens())
+            {
+                Console.WriteLine(child.Kind().ToString() + ": " + child.Text);
+            }
         }
     }
 }
