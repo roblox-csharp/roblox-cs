2b12e58543a26e07c00297f9b1452d3e60fb5631
R-unic <roleyspersonal@gmail.com>
2024-07-07 23:30:29 -0400

    feat: compile parameter lists, literals, field declarations, and constructor declarations

--- RobloxCS/CodeGenerator.cs
+++ RobloxCS/CodeGenerator.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System.Linq;
+using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
 
@@ -9,6 +9,12 @@ namespace RobloxCS
 {
     internal class CodeGenerator : CSharpSyntaxWalker
     {
+        private readonly List<SyntaxKind> _memberParentSyntaxes = new List<SyntaxKind>([
+            SyntaxKind.NamespaceDeclaration,
+            SyntaxKind.ClassDeclaration,
+            SyntaxKind.StructDeclaration
+        ]);
+
         private readonly StringBuilder _output = new StringBuilder();
         private readonly string _source;
         private readonly int _indentSize;
@@ -25,6 +31,40 @@ namespace RobloxCS
             var root = CSharpSyntaxTree.ParseText(_source).GetRoot();
             Visit(root);
             return _output.ToString();
+        }
+
+        public override void VisitLiteralExpression(LiteralExpressionSyntax node)
+        {
+            switch (node.Kind())
+            {
+                case SyntaxKind.StringLiteralExpression:
+                case SyntaxKind.Utf8StringLiteralExpression:
+                case SyntaxKind.CharacterLiteralExpression:
+                    {
+                        Write($"\"{node.Token.ValueText}\"");
+                        break;
+                    }
+
+                case SyntaxKind.TrueLiteralExpression:
+                case SyntaxKind.FalseLiteralExpression:
+                case SyntaxKind.NumericLiteralExpression:
+                    {
+                        Write(node.Token.ValueText);
+                        break;
+                    }
+                case SyntaxKind.NullLiteralExpression:
+                    {
+                        Write("nil");
+                        break;
+                    }
+
+                case SyntaxKind.DefaultLiteralExpression:
+                    {
+                        throw new Exception("\"default\" keyword is not supported!");
+                    }
+            }
+
+            base.VisitLiteralExpression(node);
         }
 
         public override void VisitNamespaceDeclaration(NamespaceDeclarationSyntax node)
@@ -59,7 +99,7 @@ namespace RobloxCS
         public override void VisitClassDeclaration(ClassDeclarationSyntax node)
         {
             var parentNamespace = FindFirstAncestor<NamespaceDeclarationSyntax>(node);
-            string namespaceName = string.Join(".", GetNames(parentNamespace));
+            var namespaceName = string.Join(".", GetNames(parentNamespace));
             var className = GetNames(node).First();
             var luaClassName = namespaceName + (namespaceName == "" ? "" : ".") + className;
             Write(luaClassName);
@@ -67,16 +107,124 @@ namespace RobloxCS
             _indent++;
 
             WriteLine($"local {className} = {luaClassName}");
-            if (!HasSyntax(node.Modifiers, SyntaxKind.StaticKeyword))
+            var isStatic = HasSyntax(node.Modifiers, SyntaxKind.StaticKeyword);
+            if (!isStatic)
             {
                 WriteLine($"{className}.__index = {className}");
             }
 
-            PrintChildNodes(node);
-            base.VisitClassDeclaration(node);
+            WriteLine();
+            var staticFields = node.Members.OfType<FieldDeclarationSyntax>()
+                .Where(member => member.Modifiers.Any(modifier => modifier.IsKind(SyntaxKind.StaticKeyword)));
+
+            InitializeDefaultFields(staticFields);
+
+            var constructor = node.Members.OfType<ConstructorDeclarationSyntax>().FirstOrDefault();
+            if (!isStatic)
+            {
+                if (constructor == null)
+                {
+                    CreateDefaultConstructor(node);
+                }
+                else
+                {
+                    VisitConstructorDeclaration(constructor);
+                }
+            }
 
             _indent--;
             WriteLine("end");
+        }
+
+        public override void VisitParameterList(ParameterListSyntax node)
+        {
+            Write("(");
+            foreach (var parameter in node.Parameters)
+            {
+                Write(GetNames(parameter).First());
+                if (parameter != node.Parameters.Last())
+                {
+                    Write(",");
+                }
+            }
+            WriteLine(")");
+            _indent++;
+
+            foreach (var parameter in node.Parameters)
+            {
+                if (parameter.Default == null) continue;
+
+                var name = GetNames(parameter).First();
+                Write(name);
+                Write(" = ");
+                Write($"if {name} == nil then ");
+                Visit(parameter.Default);
+                WriteLine($" else {name}");
+            }
+
+            _indent--;
+        }
+
+        public override void VisitConstructorDeclaration(ConstructorDeclarationSyntax node)
+        {
+            // TODO: struct support?
+            var parentClass = FindFirstAncestor<ClassDeclarationSyntax>(node)!;
+            var parentName = GetNames(parentClass).First();
+
+            Write($"function {parentName}.new");
+            Visit(node.ParameterList);
+            _indent++;
+
+            VisitConstructorBody(parentClass);
+
+            _indent--;
+            WriteLine("end");
+        }
+
+        private void VisitConstructorBody(ClassDeclarationSyntax parentClass)
+        {
+            var parentName = GetNames(parentClass).First();
+            WriteLine($"local self = setmetatable({{}}, {parentName})");
+            if (parentClass != null)
+            {
+                var nonStaticFields = parentClass.Members.OfType<FieldDeclarationSyntax>()
+                    .Where(member => member.Modifiers.All(modifier => !modifier.IsKind(SyntaxKind.StaticKeyword)));
+
+                InitializeDefaultFields(nonStaticFields); // TODO: structs
+            }
+            WriteLine("return self");
+        }
+
+        private void CreateDefaultConstructor(ClassDeclarationSyntax node)
+        {
+            string parentName = GetNames(node).First();
+            WriteLine($"function {parentName}.new()");
+            _indent++;
+
+            VisitConstructorBody(node);
+
+            _indent--;
+            WriteLine("end");
+        }
+
+        private void InitializeDefaultFields(IEnumerable<FieldDeclarationSyntax> fields)
+        {
+            foreach (var field in fields)
+            {
+                var parentClass = FindFirstAncestor<ClassDeclarationSyntax>(field)!;
+                var parentName = GetNames(parentClass).First();
+                var isStatic = HasSyntax(field.Modifiers, SyntaxKind.StaticKeyword);
+
+                foreach (var declarator in field.Declaration.Variables)
+                {
+                    if (declarator.Initializer == null) continue;
+
+                    var name = GetNames(declarator).First();
+                    Write($"{(isStatic ? parentName : "self")}.{name} = ");
+                    Visit(declarator.Initializer);
+                    WriteLine();
+                }
+            }
         }
 
         private void WriteLine(string? text = null)
@@ -99,7 +247,7 @@ namespace RobloxCS
 
         private void WriteTab()
         {
-            _output.Append(GetLastCharacter() == '\n' ? GetTabString() : "");
+            _output.Append(MatchLastCharacter('\n') ? GetTabString() : "");
         }
 
         private string GetTabString()
@@ -154,10 +302,10 @@ namespace RobloxCS
             return names;
         }
 
-        private char? GetLastCharacter()
+        private bool MatchLastCharacter(char character)
         {
-            if (_output.Length == 0) return null;
-            return _output[_output.Length - 1];
+            if (_output.Length == 0) return false;
+            return _output[_output.Length - 1] == character;
         }
 
         private void PrintChildNodes(SyntaxNode node)
--- test/src/client/Main.client.cs
+++ test/src/client/Main.client.cs
@@ -2,6 +2,11 @@ namespace TestGame.Client
 {
   public class Program
   {
+    private readonly int abc;
 
+    public Program()
+    {
+      abc = 5;
+    }
   }
 }
