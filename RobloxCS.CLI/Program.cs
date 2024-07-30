0402d37fce949917434fd93357a908731e3addf8
R-unic <roleyspersonal@gmail.com>
2024-07-30 15:59:18 -0400

    feat: add basic cli args

--- RobloxCS.CLI/Program.cs
+++ RobloxCS.CLI/Program.cs
@@ -1,12 +1,38 @@
-﻿namespace RobloxCS.CLI
+﻿using CommandLine;
+using System.Reflection;
+
+namespace RobloxCS.CLI
 {
     public static class Program
     {
+        private static readonly Assembly _compilerAssembly = Assembly.Load("RobloxCS");
+        private static readonly Version _version = _compilerAssembly.GetName().Version!;
+
+        public class Options
+        {
+            [Option('v', "version", Required = false, HelpText = "Return the compiler version.")]
+            public bool Version { get; set; }
+
+            [Value(0, Required = false, HelpText = "Project directory path", MetaName = "Path")]
+            public string? Path { get; set; }
+        }
+
         public static void Main(string[] args)
         {
-            var inputDirectory = args.FirstOrDefault(".");
-            var transpiler = new Transpiler(inputDirectory);
-            transpiler.Transpile();
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(options =>
+                {
+                    if (options.Version)
+                    {
+                           
+                        Console.WriteLine($"roblox-cs {_version.Major}.{_version.Minor}.{_version.Build}");
+                    }
+                    else
+                    {
+                        var transpiler = new Transpiler(options.Path ?? ".");
+                        transpiler.Transpile();
+                    }
+                });
         }
     }
 }
