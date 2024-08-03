d6e7d5191bc7d136d2b52391d5aa1f7cd6111443
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-08-02 22:39:54 -0300

    feat: add modifiable project name.

--- RobloxCS.CLI/Program.cs
+++ RobloxCS.CLI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using LibGit2Sharp;
 using CommandLine;
 using Spectre.Console;
@@ -25,6 +26,9 @@ namespace RobloxCS.CLI
 
             [Value(0, Required = false, HelpText = "Project directory path", MetaName = "Path")]
             public string? Path { get; set; }
+
+            [Value(1, Required = false, HelpText = "Project name", MetaName = "Name")]
+            public string? Name { get; set; }
         }
 
         public static void Main(string[] args)
@@ -32,6 +36,7 @@ namespace RobloxCS.CLI
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(options =>
                 {
+                    var name = options.Name ?? "Example";
                     var path = options.Path ?? ".";
                     if (options.Version)
                     {
@@ -39,7 +44,7 @@ namespace RobloxCS.CLI
                     }
                     else if (options.Init)
                     {
-                        InitializeProject(path);
+                        InitializeProject(path, name);
                     }
                     else if (options.WatchMode)
                     {
@@ -93,7 +98,7 @@ namespace RobloxCS.CLI
             }
         }
 
-        private static void InitializeProject(string path)
+        private static void InitializeProject(string path, string projectName)
         {
             try
             {
@@ -107,8 +112,44 @@ namespace RobloxCS.CLI
             }
 
             DeleteDirectoryManual(Path.Combine(path, ".git"));
-            DotNet.Command.Create("dotnet", ["restore", path]);
+            DotNet.Command.Create("dotnet", ["restore", path]).Execute();
             AnsiConsole.MarkupLine("[cyan]Successfully restored .NET project.[/]");
+
+            if (projectName != "Example")
+            {
+                var projectFolder = Path.Combine(path, projectName);
+                var projectSourceFolder = Path.Combine(projectFolder, "src");
+                var projectFile = Path.Combine(projectSourceFolder, $"{projectName}.csproj");
+                var rojoManifestFile = Path.Combine(projectFolder, "default.project.json");
+
+                AnsiConsole.MarkupLine("[yellow]Renaming project...[/]");
+                File.Delete(Path.Combine(path, "Example.sln"));
+                DotNet.Command.Create("dotnet", ["new", "sln", "-n", projectName, "-o", path]).Execute();
+                Directory.Move(Path.Combine(path, "Example"), projectFolder);
+                File.Move(Path.Combine(projectSourceFolder, "Example.csproj"), projectFile);
+
+                // Dynamicallty replace <Title>...</Title> inside of the csproj file. This sucks, and we must edit the solution to not lose the reference to it.
+                var lines = File.ReadAllText(projectFile);
+                var start = lines.IndexOf("<Title>", StringComparison.InvariantCulture) + "<Title>".Length;
+                var end = lines.IndexOf("</Title>", StringComparison.InvariantCulture);
+                lines = lines.Remove(start, end - start);
+                lines = lines.Insert(start, projectName);
+                File.WriteAllText(projectFile, lines);
+               
+                DotNet.Command.Create("dotnet", ["sln", Path.Combine(path, $"{projectName}.sln"), "add", projectFile]).Execute();
+                AnsiConsole.MarkupLine("[cyan]Successfully renamed project.[/]");
+
+                AnsiConsole.MarkupLine("[yellow]Modifying rojo manifest...[/]");
+
+                var rojoManifest = File.ReadAllText(rojoManifestFile);
+                // Match for the name JSON key and its end and replace the name of it, horrible, but works.
+                var idxOfStart = rojoManifest.IndexOf("\"name\": \"", StringComparison.InvariantCulture) + "\"name\": \"".Length;
+                var idxOfEnd = rojoManifest.IndexOf("\",", idxOfStart, StringComparison.InvariantCulture);
+                rojoManifest = rojoManifest.Remove(idxOfStart, idxOfEnd - idxOfStart);
+                rojoManifest = rojoManifest.Insert(idxOfStart, projectName);
+                File.WriteAllText(rojoManifestFile, rojoManifest);
+
+            }
 
             Console.WriteLine($"Configuration:");
             var initRepo = AnsiConsole.Confirm("\t[yellow]Do you want to initialize a Git repository?[/]", true);
