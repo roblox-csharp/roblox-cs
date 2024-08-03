46a271d6c9b4a7ff7d0ebf77adbe325abbdce79b
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2024-08-03 00:14:00 -0300

    feat: add LuauWriter and WritableTextBuffer to abstract writing syntax from CodeGenerator to a helper

---
+++ RobloxCS/CodeGeneration/LuauWriter.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RobloxCS.CodeGeneration;
+
+public class LuauWriter
+{
+    private WritableTextBuffer _buffer;
+
+    public LuauWriter()
+    {
+        _buffer = new WritableTextBuffer();
+    }
+
+    public void WriteRequire(string requirePath)
+    {
+        _buffer.WriteLine($"require({requirePath})");
+    }
+
+    public void DefineVariable(string variableName)
+    {
+        WriteVariable(variableName, null);
+    }
+
+    public void WriteVariable(string variableName, string? defaultVariableValue = null)
+    {
+        WriteVariableWithTypeAnnotation(variableName, null, defaultVariableValue);
+    }
+
+    public void WriteReturn(string variableName, TypeSyntax? type)
+    {
+        _buffer.WriteLine(
+            type == null ?
+                $"return {variableName} :: any" :
+                $"{variableName} :: {Utility.GetMappedType(type.ToString())}"
+        );
+    }
+
+    public void WriteVariableWithTypeAnnotation(string variableName, TypeSyntax? type, string? defaultVariableValue)
+    {
+        var typeNote = "";
+        if (type is { IsVar: false })
+        {
+            typeNote = $": {Utility.GetMappedType(type.ToString())}";
+        }
+
+        // Assume that defining the variable is what is intended.
+        _buffer.WriteLine(
+            defaultVariableValue == null ?
+                $"{variableName}{typeNote}" :
+                $"{variableName}{typeNote} = {defaultVariableValue}"
+        );
+    }
+}
---
+++ RobloxCS/CodeGeneration/WritableTextBuffer.cs
@@ -0,0 +1,70 @@
+﻿using System.Text;
+
+namespace RobloxCS.CodeGeneration;
+
+public class WritableTextBuffer
+{
+    private short _indentation;
+    private int _indentationSize;
+    private StringBuilder _stringBuilder;
+
+    public WritableTextBuffer()
+    {
+        _indentationSize = 4;
+        _indentation = 0;
+        _stringBuilder = new StringBuilder(2048);
+    }
+
+    private bool MatchLastCharacter(char character)
+    {
+        if (_stringBuilder.Length == 0) return false;
+        return _stringBuilder[^1] == character;
+    }
+
+    public void IncreaseIndent()
+    {
+        _indentation++;
+    }
+
+    public void DecreaseIndentation()
+    {
+        _indentation--;
+    }
+
+    private void WriteTab()
+    {
+        _stringBuilder.Append(MatchLastCharacter('\n') ? string.Concat(Enumerable.Repeat(' ', _indentationSize * _indentation)) : "");
+    }
+
+    public void WriteLine(char? text, bool applyIndentation = true)
+    {
+        if (text == null)
+            WriteLine((string?)null);
+
+        WriteLine(text.ToString(), applyIndentation);
+    }
+
+    public void WriteLine(string? text, bool applyIndentation = true)
+    {
+        if (text == null)
+        {
+            _stringBuilder.AppendLine();
+            return;
+        }
+
+        if (applyIndentation)
+            WriteTab();
+        _stringBuilder.AppendLine(text);
+    }
+
+    public void Write(char text, bool applyIndentation = true)
+        => Write(text.ToString(), applyIndentation);
+
+
+    public void Write(string text, bool applyIndentation = true)
+    {
+        if (applyIndentation)
+            WriteTab();
+        _stringBuilder.Append(text);
+    }
+}
