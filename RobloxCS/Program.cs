a1ffb4453b2e58150d210344f5a4188c2d0b6eb9
R-unic <roleyspersonal@gmail.com>
2025-01-02 18:27:13 -0500

    chore: remove readline in Program.cs

--- RobloxCS/Program.cs
+++ RobloxCS/Program.cs
@@ -10,6 +10,5 @@ public static class Program
         var source = File.ReadAllText(args[0]).Trim();
 
         Console.WriteLine(Transpiler.Transpile(source));
-        Console.ReadLine();
     }
 }
