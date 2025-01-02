90a8e619ce7cf554ff1a80b5d4f9157543ba3e05
R-unic <roleyspersonal@gmail.com>
2025-01-01 21:31:57 -0500

    feat: have Program.cs take a file path from CLI args

--- RobloxCS/Program.cs
+++ RobloxCS/Program.cs
@@ -1,32 +1,33 @@
 ﻿using Microsoft.CodeAnalysis.CSharp;
-using RobloxCS;
 using RobloxCS.Luau;
 
-var source = """
-class A {
-    public int x { get; set; } = 1;
+namespace RobloxCS;
 
-    public void Rah() {}
-}
-""".Trim();
-
-var references = FileUtility.GetCompilationReferences();
-var sourceAST = CSharpSyntaxTree.ParseText(source);
-var compiler = CSharpCompilation.Create(
-    assemblyName: "RewriteTest",
-    syntaxTrees: [sourceAST],
-    references
-);
-
-// RobloxCS.Utility.PrettyPrint(sourceAST.GetRoot().ChildNodes().First().ChildNodes().First().ChildNodes().First().ChildNodes().Last().ChildNodes().First().ChildNodes().First().ChildNodes().First());
-foreach (var diagnostic in compiler.GetDiagnostics())
+public static class Program
 {
-    if (diagnostic.Id == "CS5001") continue;
-    Logger.HandleDiagnostic(diagnostic);
+    public static void Main(string[] args)
+    {
+        var source = File.ReadAllText(args[0]).Trim();
+
+        var references = FileUtility.GetCompilationReferences();
+        var sourceAST = CSharpSyntaxTree.ParseText(source);
+        var compiler = CSharpCompilation.Create(
+            assemblyName: "RewriteTest",
+            syntaxTrees: [sourceAST],
+            references
+        );
+
+        // RobloxCS.Utility.PrettyPrint(sourceAST.GetRoot().ChildNodes().First().ChildNodes().First().ChildNodes().First().ChildNodes().Last().ChildNodes().First().ChildNodes().First().ChildNodes().First());
+        foreach (var diagnostic in compiler.GetDiagnostics())
+        {
+            if (diagnostic.Id == "CS5001") continue;
+            Logger.HandleDiagnostic(diagnostic);
+        }
+
+        var generator = new LuauGenerator(sourceAST, compiler);
+        var luauAST = generator.GetLuauAST();
+        var luau = new LuauWriter();
+
+        Console.WriteLine(luau.Render(luauAST));
+    }
 }
-
-var generator = new LuauGenerator(sourceAST, compiler);
-var luauAST = generator.GetLuauAST();
-var luau = new LuauWriter();
-
-Console.WriteLine(luau.Render(luauAST));
---
+++ RobloxCS/test.cs
@@ -0,0 +1,5 @@
+class A {
+    public int x { get; set; } = 1;
+                         
+    public void Rah() {}
+}
