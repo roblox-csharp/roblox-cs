e5f1539c92473f6bdea891d391c0254b4d7cfac1
R-unic <roleyspersonal@gmail.com>
2025-01-08 20:34:09 -0500

    test: identifier & variable generation

--- RobloxCS.Tests/GenerationTest.cs
+++ RobloxCS.Tests/GenerationTest.cs
@@ -6,13 +6,50 @@ namespace RobloxCS.Tests;
 public class GenerationTest
 {
     [Theory]
-    [InlineData("69.420", "69.42")]
-    [InlineData("420", "420")]
-    [InlineData("\"abc\"", "\"abc\"")]
-    [InlineData("'a'", "\"a\"")]
-    [InlineData("true", "true")]
-    [InlineData("false", "false")]
-    [InlineData("null", "nil")]
+    [InlineData("var a = 1;", null, "1")]
+    [InlineData("int b = 2;", "number", "2")]
+    [InlineData("string foo = \"bar\"", "string", "\"bar\"")]
+    [InlineData("bool guh;", "boolean", null)]
+    public void Generates_VariableDeclarations(string csharpSource, string? expectedLuauType, string? expectedValueText)
+    {
+        var ast = Generate(csharpSource);
+        Assert.NotEmpty(ast.Statements);
+        
+        var statement = ast.Statements.Skip(1).First();
+        Assert.IsType<VariableList>(statement);
+        
+        var variableList = (VariableList)statement;
+        var variable = variableList.Variables.First();
+        Assert.Equal(expectedLuauType, variable.Type?.ToString());
+        Assert.Equal(expectedValueText, (variable.Initializer as Literal)?.ValueText);
+    }
+    
+    [Theory]
+    [InlineData("object abc123;", "abc123")]
+    [InlineData("object @bruh;", "bruh")]
+    [InlineData("object abc;", "abc")]
+    public void Generates_Identifiers(string csharpSource, string expectedLuauIdentifier)
+    {
+        var ast = Generate(csharpSource);
+        Assert.NotEmpty(ast.Statements);
+        
+        var statement = ast.Statements.Skip(1).First();
+        Assert.IsType<VariableList>(statement);
+        
+        // shit ass C# thinks they're declarations
+        var variableList = (VariableList)statement;
+        var identifier = variableList.Variables.First().Name;
+        Assert.Equal(expectedLuauIdentifier, identifier.Text);
+    }
+    
+    [Theory]
+    [InlineData("69.420;", "69.42")]
+    [InlineData("420;", "420")]
+    [InlineData("\"abc\";", "\"abc\"")]
+    [InlineData("'a';", "\"a\"")]
+    [InlineData("true;", "true")]
+    [InlineData("false;", "false")]
+    [InlineData("null;", "nil")]
     public void Generates_Literals(string csharpValueText, string luauValueText)
     {
         var ast = Generate(csharpValueText);
