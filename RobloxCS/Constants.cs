eee348ac9c903c74bdaef10b3761861fefa65f81
R-unic <roleyspersonal@gmail.com>
2025-04-26 01:15:51 -0400

    feat: disallow more base types

--- RobloxCS/Constants.cs
+++ RobloxCS/Constants.cs
@@ -4,5 +4,5 @@ internal static class Constants
 {
     public const string HeaderComment = "Compiled with roblox-cs v2.0.0";
 
-    public static readonly HashSet<string> DISALLOWED_BASE_TYPES = ["IEnumerable"];
+    public static readonly HashSet<string> DISALLOWED_BASE_TYPES = ["Array", "IEnumerable", "ISet", "HashSet"];
 }
