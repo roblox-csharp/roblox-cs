7ff73ed77045febea4c86f03a6b869c101411e1d
R-unic <roleyspersonal@gmail.com>
2025-04-26 13:48:44 -0400

    test: primary ctor to explicit ctor transformation

--- RobloxCS.Tests/TransformerTests/MainTransformerTest.cs
+++ RobloxCS.Tests/TransformerTests/MainTransformerTest.cs
@@ -8,6 +8,57 @@ namespace RobloxCS.Tests.TransformerTests;
 public class MainTransformerTest
 {
     [Fact]
+    public void Transforms_PrimaryConstructors()
+    {
+        const string source = """
+                              class MyClass(int abc, string def)
+                              {
+                                  public int Abc { get; } = abc;
+                                  public string Def { get; } = def;
+                              }
+                              """;
+        
+        var compilationUnit = Transform(source);
+        var classDecl = compilationUnit.Members.OfType<ClassDeclarationSyntax>().Single();
+        Assert.Null(classDecl.ParameterList); // removed primary ctor
+
+        var properties = classDecl.Members.OfType<PropertyDeclarationSyntax>().ToArray();
+        Assert.Equal(2, properties.Length);
+        Assert.Contains(properties, p => p.Identifier.Text == "Abc");
+        Assert.Contains(properties, p => p.Identifier.Text == "Def");
+        Assert.All(properties, p => Assert.Null(p.Initializer)); // props using primary params have no initializer
+
+        var constructors = classDecl.Members.OfType<ConstructorDeclarationSyntax>().ToList();
+        Assert.Single(constructors);
+        
+        var constructor = constructors.First();
+        Assert.Equal("MyClass", constructor.Identifier.Text);
+
+        var parameters = constructor.ParameterList.Parameters;
+        Assert.Equal(2, parameters.Count);
+        Assert.Equal("abc", parameters[0].Identifier.Text);
+        Assert.Equal("def", parameters[1].Identifier.Text);
+
+        var statements = constructor.Body!.Statements.OfType<ExpressionStatementSyntax>().ToArray();
+        Assert.Equal(2, statements.Length);
+
+        var assignments = statements.Select(s => s.Expression).OfType<AssignmentExpressionSyntax>().ToArray();
+        Assert.All(assignments, assign =>
+        {
+            Assert.Equal(SyntaxKind.SimpleAssignmentExpression, assign.Kind());
+            Assert.IsType<MemberAccessExpressionSyntax>(assign.Left);
+            Assert.IsType<IdentifierNameSyntax>(assign.Right);
+        });
+
+        var assignedProperties = assignments.Select(a =>
+                                                        ((MemberAccessExpressionSyntax)a.Left).Name.Identifier.Text
+                                                   ).ToArray();
+
+        Assert.Contains("Abc", assignedProperties);
+        Assert.Contains("Def", assignedProperties);
+    }
+    
+    [Fact]
     public void Transforms_FileScopedNamespaces()
     {
         var compilationUnit = Transform("namespace Abc;");
