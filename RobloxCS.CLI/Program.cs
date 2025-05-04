1ddb6c6d4f430d00f142b36a8db56c6df05c5825
R-unic <roleyspersonal@gmail.com>
2025-05-04 18:02:29 -0400

    fix: -f flag (again)

--- RobloxCS.CLI/Program.cs
+++ RobloxCS.CLI/Program.cs
@@ -24,7 +24,7 @@ static void HandleOptions(Options opts)
                                                               new RojoProject(),
                                                               ConfigReader.UnitTestingConfig);
         
-        Console.WriteLine(transpiledLuau);
+        Console.WriteLine(transpiledLuau.Output);
         return;
     }
     
